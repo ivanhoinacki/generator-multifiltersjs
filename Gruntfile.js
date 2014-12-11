85cccf665b7a7afe4f98471e652f0bac4b5e73c6
Ivan Augusto Hoinacki <hi@ivanhoinacki.com>
2014-12-11 10:06:06 -0200

    Release v0.0.8

---
+++ Gruntfile.js
@@ -0,0 +1,140 @@
+module.exports = function(grunt) {
+  'use strict';
+
+	// Load the plugin that clean files and directories.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	// Load the plugin that concatenate files.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	// Load the plugin that copy files and directories.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	// A convenient plugin but not necessary because you can just use 'copy' and 'delete' tasks in Grunt.
+	// Built this more to fiddle around with Grunt and to gain some knowledge HowTo build a plugin for Grunt.
+	grunt.loadNpmTasks('grunt-contrib-rename');
+	// Load the plugin that minify and concatenate ".js" files.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Run shell commands
+	grunt.loadNpmTasks('grunt-shell');
+	// Publish to GitHub Pages with Grunt
+	grunt.loadNpmTasks('grunt-gh-pages');
+	// Bump package version, create tag, commit, push ...
+	grunt.loadNpmTasks('grunt-bump');
+	// Automatic notifications when tasks fail.
+	grunt.loadNpmTasks('grunt-notify');
+
+  grunt.initConfig({
+	  pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*!\n' +
+            ' * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2010-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+
+    /* clean directories */
+    clean: ['dist', 'src/concat.js'],
+
+    /* concat files */
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: false
+      },
+      basic_and_extras: {
+        files: {
+          'src/concat.js': ['about.js', 'src/**/*.js'],
+        },
+      },
+    },
+
+    /* put files not handled in other tasks here */
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'src/',
+          src: ['concat.js'],
+          dest: 'dist/'
+        }]
+      }
+	  /*,
+      site: {
+        files: [{
+          expand: true,
+          dot: true,
+          src: ['dist/*.js'],
+          dest: 'docs/_site'
+        }]
+      }*/
+    },
+
+    /* rename files */
+    rename: {
+      main: {
+        files: [{
+          src: ['dist/concat.js'],
+          dest: 'dist/<%= pkg.name %>.js'
+        }]
+      }
+    },
+
+    /* js file minification */
+    uglify: {
+      options: {
+        preserveComments: false
+      },
+			build: {
+				src: 'dist/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+    },
+
+    /* build jekyll */
+    shell: {
+      jekyllBuild: {
+        command: 'jekyll build --source docs --destination docs/_site'
+      }
+    },
+
+    /* commit on gh-pages github */
+    'gh-pages': {
+      options: {
+        base: 'docs/_site/',
+        message: 'auto-generated commit'
+      },
+      src: ['**/*']
+    },
+
+    /* update bower json */
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commitFiles: ['-a'],
+        push: true
+      }
+    }
+
+	});
+
+	// tasks
+  grunt.registerTask('build', [
+      'clean',
+      'concat',
+      'copy:dist',
+      'rename',
+      'uglify',
+      //'shell',
+      //'copy:site',
+      //'gh-pages',
+      'bump'
+  ]);
+
+  grunt.registerTask('deploy', [
+      'build'
+  ]);
+
+  grunt.registerTask('default', [
+      'build'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,45 @@
+MultiFilters JS
+=========
+
+Select multiple input of the same group to perform a filter that is made via js
+
+<img align="right" height="150" src="http://bower.io/img/bower-logo.png">
+
+## Bower.io
+
+This package is available on Bower.
+
+#### Installing
+
+```sh
+# install a package and add it to bower.json
+$ bower install multifiltersjs --save
+```
+
+#### Install Bower.io
+
+```sh
+$ npm install -g bower
+```
+
+Bower depends on [Node.js](http://nodejs.org/) and [npm](http://npmjs.org/). Also make sure that [git](http://git-scm.com/) is installed as some bower
+packages require it to be fetched and installed.
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Test your changes to the best of your ability.
+4. Update the documentation to reflect your changes if they add or changes current functionality.
+5. Commit your changes (`git commit -am 'Added some feature'`)
+6. Push to the branch (`git push origin my-new-feature`)
+7. Create new Pull Request
+
+## Creator
+
+Created by Ivan Augusto Hoinacki
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
